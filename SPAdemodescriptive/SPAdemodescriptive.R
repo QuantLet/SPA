4e1a897dd918ec38d83c308b5be05c16bc41a76d
Lei Fang <LFlvb@users.noreply.github.com>
2015-11-24 14:13:25 +0100

    Update SPAdemodescriptive.R

--- SPAdemodescriptive/SPAdemodescriptive.R
+++ SPAdemodescriptive/SPAdemodescriptive.R
@@ -18,22 +18,21 @@ lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 # Japan mortality data
 par(mar = c(5, 5, 2, 2), cex.axis = 1.5, cex.lab = 2)
 japan1 = read.demogdata("Mx_1x1.txt", "Exposures_1x1.txt", type = "mortality", label = "Japan")
-plot(japan1, series = "male", main = "", ylim = c(-10, 2), xlim = c(0, 110), ylab = "Log death rate (male)")
-plot(japan1, series = "female", main = "", ylim = c(-10, 2), xlim = c(0, 110), ylab = "Log death rate (female)")
+plot(japan1, series = "male", ylim = c(-10, 2), xlim = c(0, 110), ylab = "Log death rate (male)")
+plot(japan1, series = "female", ylim = c(-10, 2), xlim = c(0, 110), ylab = "Log death rate (female)")
 
 # fertility data
 japan.fert = read.demogdata("JPNasfrRR.txt", "JPNexposRR.txt", type = "fertility", 
                             label = "Japan")
-plot(japan.fert, main = "", ylim = c(0, 0.3), xlim = c(10, 55))
+plot(japan.fert, ylim = c(0, 0.3), xlim = c(10, 55))
 
 # Taiwan mortality data
 taiwan1 = read.demogdata("Mx_1x1tw.txt", "Exposures_1x1tw.txt", type = "mortality", 
-                         label = "taiwan")
-plot(taiwan1, series = "male", main = "", ylim = c(-10, 2), xlim = c(0, 110), ylab = "Log death rate (male)")
-plot(taiwan1, series = "female", main = "", ylim = c(-10, 2), xlim = c(0, 110), ylab = "Log death rate (female)")
+                         label = "Taiwan")
+plot(taiwan1, series = "male", ylim = c(-10, 2), xlim = c(0, 110), ylab = "Log death rate (male)")
+plot(taiwan1, series = "female", ylim = c(-10, 2), xlim = c(0, 110), ylab = "Log death rate (female)")
 
 # fertility data
 taiwan.fert = read.demogdata("TWNasfrRR.txt", "TWNexposRR.txt", type = "fertility", 
-                             label = "taiwan")
-plot(taiwan.fert, main = "", ylim = c(0, 0.3), xlim = c(10, 55)) 
-
+                             label = "Taiwan")
+plot(taiwan.fert, ylim = c(0, 0.3), xlim = c(10, 55)) 
